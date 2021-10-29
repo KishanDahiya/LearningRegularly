4191faa3c25820e78410f97437628fd3e7eac28a
Tzuyu's Forgotten Luggage <84631221+TzuyusForgottenLuggage@users.noreply.github.com>
2021-10-30 01:21:17 +0530

    updated progress

--- Java DSA/TypeCasting.java
+++ Java DSA/TypeCasting.java
@@ -1,10 +1,11 @@
 public class TypeCasting{
     public static void main(String[] args){
         // Link : https://www.geeksforgeeks.org/type-conversion-java-examples/
+
         System.out.println(Math.pow(2, 3));
         double exp = Math.pow(2, 3);
-        int r = (int)exp; //Narrowing
-        double rr = r; //Widenning
+        int r = (int)exp; //Narrowing means converting from larger datatype to smaller datatype.
+        double rr = r; //Widenning means converting from smaller datatype to larger datatype.
         System.out.println(rr);
     }
 }
