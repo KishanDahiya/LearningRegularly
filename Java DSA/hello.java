9ccd3b3168ccd792dd0cf674b399dfec2c5b4680
Tzuyu's Forgotten Luggage <84631221+TzuyusForgottenLuggage@users.noreply.github.com>
2021-10-29 16:25:56 +0530

    Starting DSA with JAVA

---
+++ Java DSA/hello.java
@@ -0,0 +1,8 @@
+public class hello {
+
+    public static void main(String[] args) {
+        String s="hello";
+        System.out.println(s);
+    }
+}
+/* Well Lets do this shall we , Begining DSA with JAVA*/
