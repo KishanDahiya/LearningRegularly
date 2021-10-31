9785593095023402c69099b5f13f4d4f4f84ae5f
Tzuyu's Forgotten Luggage <84631221+TzuyusForgottenLuggage@users.noreply.github.com>
2021-10-31 22:33:21 +0530

    updated progress

---
+++ Java DSA/Conditions.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Conditions {
+    public static void main(String[] args){
+        System.out.println("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+
+        if (x % 2 == 0) {
+        System.out.println("The number is even");
+        } else {
+        System.out.println("The number is odd");
+        }
+        System.out.println("\nHardwork is better than smart work");
+        }
+    
+}
---
+++ Java DSA/Variables.java
@@ -0,0 +1,12 @@
+public class Variables{
+    public static void main(String[] args){
+        int a = 10; 
+        int b = 20;
+        a = a + b;
+        System.out.println(a+" "+b);
+        b = a - b;
+        System.out.println(a+" "+b);
+        a = a - b;
+        System.out.println(a+" "+b);
+    }
+}
